662895bb7639b8c48fd29b92c3a5b7735adfbdc2
vanessaoja <ojavanessa@gmail.com>
2022-06-28 17:30:15 -0500

    enemy attacked

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,13 +13,16 @@ var fight = function() {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
-  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+  //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable 
   enemyHealth = enemyHealth - playerAttack;
 
   // Log a resulting message to the console so we know that it worked.
-  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
  
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
   // Log a resulting message to the console so we know that it worked.
 };
+
+fight();
