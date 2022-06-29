1c82a5a00be23940cfe60a600babd1343b39a51e
vanessaoja <ojavanessa@gmail.com>
2022-06-29 14:56:51 -0500

    roberto died

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,7 +6,7 @@ var playerAttack = 10;
 console.log(playerName, playerAttack, playerHealth);
 
 var enemyName = "Roborto";
-var enemyHealth = 52;
+var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function() {
@@ -20,6 +20,13 @@ var fight = function() {
   console.log(
   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
  
+  // check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} 
+else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
 
   // Log a resulting message to the console so we know that it worked.
